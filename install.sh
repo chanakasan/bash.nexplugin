23145538fb099bb20d4df2b914bc323f769d5dbf
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-01-14 12:22:12 +0530

    revision

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@ set -e
 main() {
   local bashrc="$HOME/.bashrc"
   echo "Chk Dotfiles - Installation"
-  echo "" >> .bashrc
+
+  #-- copy
+  echo "" >> $bashrc
   echo 'export chk_dotfiles_path=$HOME/dotfiles' >> $bashrc
   echo 'export chk_bash_dist_path=$chk_dotfiles_path/chk-bash/dist' >> $bashrc
   echo 'source $chk_bash_dist_path/main' >> $bashrc
+  #--
+
   echo "Done."
 }
 
