b4fa0b0e136e51390a004e1a3e9b6c446f809348
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-01-27 23:21:37 -0800

    WIP

--- install.sh
+++ install.sh
@@ -2,16 +2,66 @@
 
 set -e
 
-main() {
+repo_base_url() {
+  #echo "https://github.com/chanakasan"
+  echo "git@github.com:chanakasan"
+}
+
+install_vim() {
+  cd $HOME/dotfiles
+  local base_url=$(repo_base_url)
+  git clone $base_url/chk-vim.git
+  cd $HOME
+  symlink_vim
+  echo "Vim installed"
+}
+
+symlink_vim() {
+  ln -nfs $HOME/dotfiles/chk-vim/_vimrc $HOME/.vimrc
+  ln -nfs $HOME/dotfiles/chk-vim/_vim $HOME/.vim
+}
+
+symlink_git() {
+  ln -nfs $HOME/dotfiles/chk-config/_gitconfig $HOME/.gitconfig
+  ln -nfs /home/chk/dotfiles/chk-config/_gitignore_global $HOME/.gitignore_global
+}
+
+symlink_tmux() {
+  ln -nfs $HOME/dotfiles/chk-mux/index.conf $HOME/.tmux.conf
+}
+
+install_tmux() {
+  cd $HOME
   local bashrc="$HOME/.bashrc"
-  echo "Chk Dotfiles - Installation"
+  ln -nfs $HOME/dotfiles/chk-mux/index.conf $HOME/.tmux.conf
+  echo 'export PATH=$chk_dotfiles_path/chk-bin:$PATH' >> $bashrc
+  symlink_tmux
+  echo "Tmux installed"
+}
+
+install_git() {
+  symlink_git
+  echo "Git installed"
+}
+
+main() {
+  local wd=$HOME
+  local bashrc="$HOME/.bashrc"
+
+  cd $wd
+  echo "Dotfiles - Installation"
 
   #-- copy
   echo "" >> $bashrc
   echo 'export chk_dotfiles_path=$HOME/dotfiles' >> $bashrc
   echo 'export chk_bash_dist_path=$chk_dotfiles_path/chk-bash/dist' >> $bashrc
   echo 'source $chk_bash_dist_path/main' >> $bashrc
+  echo "" >> $bashrc
   #--
+
+  install_git
+  install_tmux
+  install_vim
 
   echo "Done."
 }
