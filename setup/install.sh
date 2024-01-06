82830fed9a0be378c9543ba0665e83bfc8de8d56
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-06 14:32:26 +0530

    clean

--- setup/install.sh
+++ setup/install.sh
@@ -2,63 +2,35 @@
 
 set -e
 
-get_dotfiles_path() {
+get_base_path() {
   local user=$(whoami)
   if [ "$user" = "codespace" ]; then
-    echo /workspaces/.codespaces/.persistedshare/dotfiles
+    echo /workspaces/.codespaces/.persistedshare
   else
-    echo $HOME/dotfiles
+    echo $HOME
   fi
 }
 
 remove_from_bashrc() {
-  sed -i '/#__dotfiles_start/,/#__dotfiles_end/{d}' $bashrc
+  sed -i '/#__nex_bash_start/,/#__nex_bash_end/{d}' $bashrc
 }
 
 copy_to_bashrc() {
-  local df_path=$(get_dotfiles_path)
+  local base_path=$(get_base_path)
   echo "" >> $bashrc
-  echo '#__dotfiles_start' >> $bashrc
-  echo 'export chk_dotfiles_path='$df_path >> $bashrc
-  echo 'export chk_bash_path=$chk_dotfiles_path/bash' >> $bashrc
-  echo 'source $chk_bash_path/main' >> $bashrc
-  echo 'source $chk_bash_path/optional' >> $bashrc
-  echo ''
-  echo 'export PATH=$chk_dotfiles_path/bin:$PATH' >> $bashrc
-  echo 'export PATH=$chk_dotfiles_path/mux:$PATH' >> $bashrc
-  echo 'export PATH=$chk_dotfiles_path/tmux/bin:$PATH' >> $bashrc
-  echo '#__dotfiles_end' >> $bashrc
+  echo '#__nex_bash_start' >> $bashrc
+  echo 'export nex_bash_path='$base_path/nex-bash >> $bashrc
+  echo 'source $nex_bash_path/src/main' >> $bashrc
+  echo 'export PATH=$nex_bash_path/bin:$PATH' >> $bashrc
+  echo '#__nex_bash_end' >> $bashrc
   echo "" >> $bashrc
-}
-
-start_debug() {
-  HOME=$hq/box1
-}
-
-mark_installed() {
-  touch $installed_file
-}
-
-check_installed() {
-  if [ -f "$installed_file" ]; then
-    echo "Error: Already installed"
-    echo "Aborted"
-    exit 255
-  fi
-}
-
-do_steps() {
-  remove_from_bashrc
-  copy_to_bashrc
 }
 
 main() {
-  # start_debug
-  echo "Dotfiles - Installation"
-  local root=$HOME
-  local bashrc="$root/.bashrc"
-  local installed_file=$root/.chk_dotfiles_installed
-  do_steps
+  local bashrc="$HOME/.bashrc"
+  echo "Nex Bash Install"
+  remove_from_bashrc
+  copy_to_bashrc
   echo "Done"
   echo "Please run 'reload'"
   echo ""
--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -2,34 +2,15 @@
 
 set -e
 
-start_debug() {
-  HOME=/c/Bash/test1
-}
-
-check_installed() {
-  if [ ! -f "$installed_file" ]; then
-    echo "Error: Not installed"
-    echo "Aborted"
-    exit 255
-  fi
-}
-
 remove_from_bashrc() {
   sed -i '/#__dotfiles_start/,/#__dotfiles_end/{d}' $bashrc
 }
 
-do_steps() {
+main() {
+  echo "Nex Bash - Uninstall"
+  local bashrc=$HOME/.bashrc
   check_installed
   remove_from_bashrc
-  rm $installed_file
-}
-
-main() {
-  # start_debug
-  echo "Dofiles - Uninstall"
-  local bashrc=$HOME/.bashrc
-  local installed_file=$HOME/.chk_dotfiles_installed
-  do_steps
   echo "Done"
   echo ""
 }
