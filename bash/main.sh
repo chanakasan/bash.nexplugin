2797c97b05380207fdd73358edcd40eddeb1c714
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-03-30 06:00:16 +0530

    rev

--- bash/main.sh
+++ bash/main.sh
@@ -3,6 +3,5 @@ source $nex_bash_path/lib/utils.sh
 #[[ $- == *i* ]] && stty -ixon
 
 _nx_source_main_files
-_nx_source_umods
 
 echo "Bash loaded."
--- lib/utils.sh
+++ lib/utils.sh
@@ -3,13 +3,3 @@ _nx_source_main_files() {
     source $f
   done
 }
-
-_nx_source_umods() {
-  export nex_user_mods_path="$HOME/umods"
-  mkdir -p $nex_user_mods_path
-  touch $nex_user_mods_path/empty.bash
-
-  for f in $HOME/umods/*.bash; do
-    source $f
-  done
-}
