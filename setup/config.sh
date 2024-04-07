95ca3a66b53beda0122340cf313f26a7057983e2
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-07 19:18:17 +0530

    rev

--- setup/config.sh
+++ setup/config.sh
@@ -6,7 +6,7 @@ this_plugin_path=$root_path/$this_plugin_name
 
 # bashrc
 bashrc="$HOME/.bashrc"
-this_start_text='__nex_run_start'
-this_end_text='__nex_run_end'
+this_start_text='__nex_bash_start'
+this_end_text='__nex_bash_end'
 tpl_text=$(cat $this_dir/bashrc.tpl)
 this_middle_text="${tpl_text//__s1__/$this_plugin_path}"
