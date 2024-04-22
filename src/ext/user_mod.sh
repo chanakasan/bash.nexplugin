d0565efd80f7240960a10580975e6442c3a7e84c
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-22 12:31:27 +0530

    rev

--- src/ext/user_mod.sh
+++
@@ -1,5 +0,0 @@
-export user_mod_path=$HOME/user_mod
-tmp_source_folder $user_mod_path
-tmp_source_folder $user_mod_path/env
-tmp_source_folder $user_mod_path/alias
-tmp_source_folder $user_mod_path/mod
