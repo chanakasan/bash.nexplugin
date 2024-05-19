3de9350c8ad0dcdb107b23783d64ae9f045ad3b7
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-20 02:00:38 +0530

    rev

--- src/ext/user_bash.sh
+++
@@ -1,5 +0,0 @@
-user_bash_base=$HOME/user/bash
-tmp_source_folder $user_bash_base
-tmp_source_folder $user_bash_base/env
-tmp_source_folder $user_bash_base/alias
-tmp_source_folder $user_bash_base/func
